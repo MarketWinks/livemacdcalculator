2c64394263f74e3fbd64558777c0114a880f1b67
Chocks <chocku.engr@gmail.com>
2020-04-27 17:27:36 +0100

    initial commit

--- src/main/java/com/marketwinks/livemacdcalculator/LivemacdcalculatorApplication.java
+++ src/main/java/com/marketwinks/livemacdcalculator/LivemacdcalculatorApplication.java
@@ -1,5 +1,6 @@
 package com.marketwinks.livemacdcalculator;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
